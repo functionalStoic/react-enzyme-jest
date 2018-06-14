f3ac1869b66a1a5ddbe9f77c2b93b7ca646cb3a9
JasonSooter <jason.sooter@gmail.com>
2018-06-14 17:16:06 -0500

    Found nodes within a shallow render with multiple ways

--- src/App.js
+++ src/App.js
@@ -12,6 +12,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        {/* <ul>
+          <li>Test 1</li>
+          <li>Test 2</li>
+          <li>Test 3</li>
+        </ul>
+        <li>Test 3</li> */}
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- src/App.test.js
+++ src/App.test.js
@@ -8,11 +8,9 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
-  it('should render App', () => {
-    const wrapper = shallow(<App />, {
-      context: {},
-      disableLifecycleMethods: true
-    });
-    console.log(wrapper.debug());
+  const wrapper = shallow(<App />);
+  it('should contain h1 with the text `Welcome to React`', () => {
+    // expect(wrapper.find('ul').children().length).toBe(3);
+    expect(wrapper.find('h1').text()).toBe('Welcome to React');
   });
 });
