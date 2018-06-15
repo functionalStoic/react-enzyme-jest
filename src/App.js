ceae7d67a0d520d845af9595a898efddd3a6c39d
JasonSooter <jason.sooter@gmail.com>
2018-06-14 23:20:44 -0500

    Added all of the various ways you can target elements via css selectors, etc

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const Test = () => <div>Testing</div>;
-
+const Title = ({ text }) => <div>{text}</div>;
 class App extends Component {
   render() {
     return (
@@ -12,16 +11,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        {/* <ul>
-          <li>Test 1</li>
-          <li>Test 2</li>
-          <li>Test 3</li>
-        </ul>
-        <li>Test 3</li> */}
+        <Title id="TitleComponent" text="Some title" />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Test />
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -9,8 +9,29 @@ configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
   const wrapper = shallow(<App />);
-  it('should contain h1 with the text `Welcome to React`', () => {
-    // expect(wrapper.find('ul').children().length).toBe(3);
+  it('should contain match one of the css selectors', () => {
+    // Various ways to target an element vis css selector
+    // Target via element
     expect(wrapper.find('h1').text()).toBe('Welcome to React');
+    // Target via class
+    expect(wrapper.find('.App-intro').text()).toContain('To get started');
+    // Target via id
+    expect(wrapper.find('#TitleComponent').text()).toBe('<Title />');
+    // Target via attribute
+    expect(wrapper.find('[alt="logo"]').exists()).toBe(true);
+    // Target via combo of element and attribute
+    expect(wrapper.find('img[alt="logo"]').exists()).toEqual(true);
+    // Target via child selector
+    expect(wrapper.find('.App > .App-intro').text()).toContain('To get');
+    // Target via adjacent selector
+    expect(wrapper.find('img + h1').text()).toContain('Welcome');
+    // Target via general sibling selector
+    expect(wrapper.find('img ~ .App-title').text()).toContain('Welcome');
+    // Target via descendant selector
+    expect(wrapper.find('.App .App-intro').text()).toContain('To get');
+    // Target via component prop
+    expect(wrapper.find('[text="Some title"]').text()).toBe('<Title />');
+    // Target via object that matches properties of an element
+    expect(wrapper.find({ alt: 'logo' }).exists()).toBe(true);
   });
 });
