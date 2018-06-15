f728819cd2cc5128c6a86727d16a2adb6eba6804
JasonSooter <jason.sooter@gmail.com>
2018-06-14 23:29:04 -0500

    Added test to create snapshot of App component - added toJson package to make a clean snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
 
 configure({ adapter: new Adapter() });
 
@@ -33,5 +34,9 @@ describe('<App />', () => {
     expect(wrapper.find('[text="Some title"]').text()).toBe('<Title />');
     // Target via object that matches properties of an element
     expect(wrapper.find({ alt: 'logo' }).exists()).toBe(true);
+  });
+  it('matches the snapshot', () => {
+    const tree = shallow(<App />);
+    expect(toJson(tree)).toMatchSnapshot();
   });
 });
